bedba1c105cacd277d3ae8e9c85b4f98eb00a1a6
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-29 16:32:50 +0100

    connect database successfully

--- Api/server.js
+++ Api/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+//Mongoodb connection
+const connectDB=require('./config/db')
+connectDB()
+
+
 app.listen(PORT, () => {
   console.log(`Server connecting ${PORT}`);
 });
