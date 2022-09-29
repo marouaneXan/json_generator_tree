9b36a948a3386144e54d257bde615eaf17d5e401
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-29 20:07:54 +0100

    add routes for all pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route/>
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/dashboard" element={<Dashboard/>}/>
         </Routes>
       </div>
     </Router>
--- Client/src/components/layouts/Navbar.jsx
+++ Client/src/components/layouts/Navbar.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { FaUser } from "react-icons/fa";
+import { FiLogIn } from "react-icons/fi";
+import { BiLogOut } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="bg-[rgb(31,41,55)]">
+      {/* <div className="text-white container flex justify-between items-center p-4 mx-auto">
+        <Link to={"/"}>
+          <h5 className="text-xl font-semibold">GoalSetter</h5>
+        </Link>
+        {!connected ? (
+          <div>
+            <Link
+              to={"/login"}
+              className="font-medium text-sm px-5 py-2.5 text-center hover:text-gray-400 inline-flex items-center"
+            >
+              <FiLogIn className="mr-1" />
+              Login
+            </Link>
+            <Link
+              to={"/"}
+              className="font-medium text-sm px-5 py-2.5 text-center hover:text-gray-400 inline-flex items-center"
+            >
+              <FaUser className="mr-1" />
+              Register
+            </Link>
+          </div>
+        ) : (
+          <button
+          type="button"
+          onClick={logout}
+            className="font-medium text-sm px-5 py-2.5 text-center hover:text-gray-400 inline-flex items-center"
+          >
+            <BiLogOut className="mr-1" />
+            Logout
+          </button>
+        )}
+      </div> */}
+    </nav>
   )
 }
 
