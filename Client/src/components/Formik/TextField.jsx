e32a35016f0a17cb23bdf7f2e468c876c37760cd
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-29 21:46:19 +0100

    fix some error for formik field

--- Client/src/components/Formik/TextField.jsx
+++ Client/src/components/Formik/TextField.jsx
@@ -13,7 +13,7 @@ const TextField = ({ label, ...props }) => {
       <input
         {...field}
         {...props}
-        className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+        className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:text-white  ${
           meta.touched && meta.error && "border-red-500"
         }`}
       />
