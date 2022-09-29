996c934ee2c820ae3842b5fe803bf03530077e7a
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-29 16:31:02 +0100

    finish connectDB function

--- Api/config/db.js
+++ Api/config/db.js
@@ -1,5 +1,11 @@
-const mongoose=require('mongoose')
-const connectDB=async()=>{
-
-}
-module.exports=connectDB
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`mongoDB connected:${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
