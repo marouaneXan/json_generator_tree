740b3247b8bdadf2ae0c2e29c796604a17125b0e
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-29 16:23:25 +0100

    import mongoose in db.js

--- Api/server.js
+++ Api/server.js
@@ -1,5 +1,7 @@
-const express =require('express')
-const dotenv=require('dotenv').config()
-const PORT =process.env.PORT || 5000
-const app=express()
-app.listen(PORT,()=>{console.log(`Server connecting ${PORT}`)})
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Server connecting ${PORT}`);
+});
