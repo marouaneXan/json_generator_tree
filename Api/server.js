262b4b6757bcafd50f949c44616c2ff2a1ff16b8
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-30 15:23:47 +0100

    import errorHandler in server.js

--- Api/server.js
+++ Api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors=require('cors')
+const {ErrorHandler}=require('./Middleware/ErrorMiddleware')
 const PORT = process.env.PORT || 5000;
 const app = express();
 
