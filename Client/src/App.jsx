edc5bc935a196fc1163efd4fe02245aa7b87cb35
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-30 16:31:07 +0100

    init state in app.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,19 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 import { useState } from "react";
 function App() {
-  const [user,setUser]=useState(localStorage.getItem('user'))
+  const [id, setId] = useState(localStorage.getItem("id"));
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   return (
     <Router>
       <div className="App">
-        <Navbar user={user}/>
+        <Navbar user={user} />
         <Routes>
-          <Route path="/register" element={<Register/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={
+              <Login
+                id={id}
+                setId={setId}
+                success={success}
+                setSuccess={setSuccess}
+                error={error}
+                setError={setError}
+                loading={loading}
+                setLoading={setLoading}
+              />
+            }
+          />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
