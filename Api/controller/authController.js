c88b5fbbfdac7776d96428946d2e170f4ca90ebf
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-30 15:03:19 +0100

    some changes

--- Api/controller/authController.js
+++ Api/controller/authController.js
@@ -1,14 +1,15 @@
 const bcryptjs=require('bcryptjs')
-const User = require("../Model/userModel");
+const User = require("../Model/userModel");o_
 
 //@desc POST register
 //@route /api/v1/auth/register
 //@access public
 const register=async(req,res)=>{
     const {name,email,password}=req.body
-    if(!name || !email || !password){
-        res.status(400).json('Please add all fields')
-    }
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please add all Fields");
+      }
 }
 module.exports={
     register
--- Api/models/userModel.js
+++ Api/models/userModel.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'Please add a name']
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
     },
-    email:{
-        type:String,
-        required:[true,'Please add a email'],
-        unique:true
+    email: {
+      type: String,
+      required: [true, "Please add a email"],
+      unique: true,
     },
-    password:{
-        type:String,
-        required:[true,'Please add a password']
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
     },
     role: {
-        type: Boolean,
-        required: true,
+      type: Boolean
     },
-}, { timestapms: true });
-module.exports = mongoose.model('User', userSchema);
+  },
+  { timestapms: true }
+);
+module.exports = mongoose.model("User", userSchema);
