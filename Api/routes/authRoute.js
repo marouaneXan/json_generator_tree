5561824e4cdc7276ad3c95c6d54c33ec9f66f1da
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-30 11:34:45 +0100

    import cors in server.js

--- Api/routes/authRoute.js
+++ Api/routes/authRoute.js
@@ -1,3 +1,7 @@
 const express=require('express')
 const router = express.Router();
+
+router.post('/register',(req,res)=>{
+    res.send(req.text)
+})
 module.exports = router;
--- Api/server.js
+++ Api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors=require('cors')
 const PORT = process.env.PORT || 5000;
 const app = express();
 
