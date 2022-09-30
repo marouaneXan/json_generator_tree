6e4fd9177e2feefdac7e2b18ebf6c0d2ee1c4a88
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-30 15:42:20 +0100

    hash the password

--- Api/controller/authController.js
+++ Api/controller/authController.js
@@ -1,4 +1,4 @@
-const bcryptjs = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
@@ -11,11 +11,15 @@ const register = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add all Fields");
   }
-  const userAlready=await User.findOne({email})
-  if(userAlready){
-    res.status(400)
-    throw new Error('User Already exist')
+  const userAlready = await User.findOne({ email });
+  if (userAlready) {
+    res.status(400);
+    throw new Error("User Already exist");
   }
+  
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 });
 module.exports = {
   register,
